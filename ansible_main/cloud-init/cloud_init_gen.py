b5a86f3eeb3b9af89d57d44f8b66cd6b0ca64119
gimmymansui <114517092+gimmymansui@users.noreply.github.com>
2024-10-09 19:19:16 -0400

    updated cloud_init_gen.py
    Included sudo package installation and ssh and user setup

--- ansible_main/cloud-init/cloud_init_gen.py
+++ ansible_main/cloud-init/cloud_init_gen.py
@@ -4,13 +4,33 @@ def generate_cloud_init_yaml(requirements_path, output_path):
         requirements = req_file.read()
 
     # Create the cloud-init YAML content
+
+    # cloud-init config file
     yaml_content = f"""#cloud-config
+users:
+  - name: user
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    ssh_authorized_keys:
+      - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNeLRIWWuEqoZ8mHZb8Q6xSqTzb2aFymxuzXBN12Jzn suij2@DESTRUCTION-SUI # CHANGE THIS SSH KEY
+
+ssh_pwauth: false
 
 package_update: true
 package_upgrade: true
 
 packages:
   - python3-pip
+  - openssh-server
+  - nmap
+  - openssh-client
+  - python3
+  - python3-pip
+  - ansible
+  - git
+  - curl
+  - wget
+  - vim
+
 
 write_files:
   - path: /requirements.txt
