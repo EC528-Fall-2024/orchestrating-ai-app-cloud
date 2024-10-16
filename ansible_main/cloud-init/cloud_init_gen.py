4d31da3788bb9193f38d1baba435aafa743e5f39
gimmymansui <114517092+gimmymansui@users.noreply.github.com>
2024-10-16 13:51:44 -0400

    More robust cloud_init_gen.py

--- ansible_main/cloud-init/cloud_init_gen.py
+++ ansible_main/cloud-init/cloud_init_gen.py
@@ -1,7 +1,16 @@
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+requirements_path = os.path.join(current_dir, 'requirements.txt')
+
+# CHANGE THIS SSH KEY
+ssh_key = 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFbUbFFMOV4SKX3B5Jo/1tWXa6kNhRdLoGpQtTB7/uuG suijs@bu.edu'
+
+
 def generate_cloud_init_yaml(requirements_path, output_path):
     # Read the requirements.txt file
     with open(requirements_path, 'r') as req_file:
-        requirements = req_file.read()
+        requirements = [line.strip() for line in req_file if line.strip() and not line.startswith('#')]
 
     # Create the cloud-init YAML content
 
@@ -11,7 +20,7 @@ users:
   - name: user
     sudo: ALL=(ALL) NOPASSWD:ALL
     ssh_authorized_keys:
-      - ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFNeLRIWWuEqoZ8mHZb8Q6xSqTzb2aFymxuzXBN12Jzn suij2@DESTRUCTION-SUI # CHANGE THIS SSH KEY
+      - {ssh_key}
 
 ssh_pwauth: false
 
@@ -32,24 +41,14 @@ packages:
   - vim
 
 
-write_files:
-  - path: /requirements.txt
-    content: |
-{requirements.strip().replace('\n', '\n      ')}
-
 runcmd:
-  - pip3 install -r /requirements.txt
-  - echo "cloud-init config complete" >> /var/log/cloud-init-output.log
-  - systemctl restart sshd
-  - ufw allow OpenSSH
-  - ufw --force enable
+  - sudo pip3 install {' '.join(requirements)}
 
 """
     # Write the cloud-init YAML file
     with open(output_path, 'w') as config_file:
         config_file.write(yaml_content)
 
-# Usage
-requirements_path = 'requirements.txt' # change this to the path of the requirements.txt file
-output_path = 'cloud-init-config.yaml'
+
+output_path = os.path.join(current_dir, 'cloud-init-config.yaml')
 generate_cloud_init_yaml(requirements_path, output_path)
