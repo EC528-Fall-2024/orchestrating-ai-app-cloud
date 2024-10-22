6c5578504258d2391477b6d549bb6e74d43b1b41
Krishna Patel <krispat@bu.edu>
2024-10-22 09:43:06 -0400

    authenticate user according to creds folder

--- cynthus_cli/cynthus/commands.py
+++ cynthus_cli/cynthus/commands.py
@@ -149,6 +149,9 @@ def containerize_project(project_path):
 
 
 def project_push(image_name):
+    if (os.name == 'nt'):
+        subprocess.run(["Get-Content", "creds\cynthusgcp-registry.json", "|", "docker",
+                       "login", "-u", "_json_key", "--password-stdin", "https://us-east4-docker.pkg.dev"])
     subprocess.run(["docker", "tag", str(
         image_name), f"us-east4-docker.pkg.dev/cynthusgcp-438617/cynthus-images/{image_name}"])
 
