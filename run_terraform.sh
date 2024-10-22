f4472a9664ed1b4cf8aab63223e062945946dce3
Jimmy <suijs@bu.edu>
2024-10-22 20:41:28 +0000

    update

---
+++ run_terraform.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build the Docker images
+sudo docker compose -f docker_terraform_compose.yaml build
+
+# Initialize Terraform
+sudo docker compose -f docker_terraform_compose.yaml run --rm terraform init
+
+# Plan Terraform changes
+sudo docker compose -f docker_terraform_compose.yaml run --rm terraform plan
+
+# Optionally, you can add the apply command here if you want to run it as well
+sudo docker compose -f docker_terraform_compose.yaml run --rm terraform apply -auto-approve
