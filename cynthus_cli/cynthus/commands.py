32c567d225268e3b877cc0075ef68f0bf16e4319
darrowball13 <darrowry@bu.edu>
2024-11-03 09:29:08 -0500

    Small changes to structure

--- cynthus_cli/cynthus/commands.py
+++ cynthus_cli/cynthus/commands.py
@@ -153,47 +153,49 @@ def cli_entry_point():
     # parser_push.add_argument(
     #     'registry', help='The name of the registry')
 
+    # Authenticate User Command (Uninitialized Currently)
+
     parser_auth = subparsers.add_parser(
         'ssh', help='Authenticate into a specified cloud registry')
     parser_auth.add_argument(
-        'ssh_key',
-        help='The public key of the user'
-    )
+        'ssh_key', help='The public key of the user')
     parser_auth.add_argument(
-        'service',
-        help='The cloud service to authenticate into'
-    )
+        'service', help='The cloud service to authenticate into')
+    
+    # Pull datasets for upload to buckets Command
 
     parser_datapull = subparsers.add_parser(
         'datapull', help='Source a dataset from a local path or supported API')
     parser_datapull.add_argument(
-        'location_type',
-        help='local_path or url'
-    )
+        'location_type', help='local_path or url')
     parser_datapull.add_argument(
-        'location',
-        help='The local path or url to pull data from'
-    )
+        'location', help='The local path or url to pull data from')
+    
+    # Set-up Kaggle Command
 
     parser_setup_kaggle = subparsers.add_parser(
-        'setup-kaggle', help='Provide instructions for Kaggle set-up'
-    )
+        'setup-kaggle', help='Provide instructions for Kaggle set-up')
+    
+    # Download Kaggle Dataset command (***May be removed***)
 
     parser_download_kaggle = subparsers.add_parser(
-        'download-kaggle', help='Download dataset from Kaggle'
-    )
+        'download-kaggle', help='Download dataset from Kaggle')
     parser_download_kaggle.add_argument(
-        'dataset', help='The Kaggle dataset to download (e.g., username/dataset-name)'
-    )
+        'dataset', help='The Kaggle dataset to download (e.g., username/dataset-name)')
     parser_download_kaggle.add_argument(
-        'dest_path', help='The local directory where the dataset will be downloaded'
-    )
+        'dest_path', help='The local directory where the dataset will be downloaded')
+    
+    # GCP Artifact Registry Authenication Command
 
     parser_gcp_docker_auth = subparsers.add_parser(
         'gcp-docker-auth', help='Authenticate to GCP Artifact Registry (test command)')
+    
+    # Create Docker YAML file Command
 
     parser_docker_yaml_create = subparsers.add_parser(
         'docker-yaml-create', help='Create sample yaml file (test command)')
+    
+    # Adding the commands to the parser
 
     args = parser.parse_args()
 
