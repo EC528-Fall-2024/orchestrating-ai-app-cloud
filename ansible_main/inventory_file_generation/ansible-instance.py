a8c75400908e75fcd5608b56fb5f7850960ca708
Thainguyenluong <Thainguyenluong@github.com>
2024-11-20 18:43:56 -0500

    add variable file generation with FastAPI

--- ansible_main/inventory_file_generation/ansible-instance.py
+++ ansible_main/inventory_file_generation/ansible-instance.py
@@ -18,11 +18,14 @@ os.makedirs(INVENTORY_DIR, exist_ok=True)
 
 class PlaybookRequest(BaseModel):
     ip: str
+    hostname: str
 
-async def run_ansible_playbook_async(inventory_path: str):
-    #path to the ansible-playbook executable obtained using "which ansible-playbook"
+async def run_ansible_playbook_async(inventory_path: str, vars_path: str):
+    """
+    Runs the Ansible playbook asynchronously using the provided inventory file path.
+    """
     process = await asyncio.create_subprocess_exec(
-        "/path/to/executable/ansible-playbook", "-i", inventory_path, PLAYBOOK,
+        "/path/to/executable/ansible-playbook", "-i", inventory_path, PLAYBOOK,"--extra-vars", f"@{vars_path}",
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
@@ -37,7 +40,20 @@ async def run_ansible_playbook_async(inventory_path: str):
 async def run_ansible_playbook(request: PlaybookRequest):
     try:
         ip_address = request.ip
-
+        host_name = request.hostname
+        uuid = host_name.split('-')[-1]
+         # Generate the vars.yml content
+        vars_content = {
+            "docker_image_name_src": f"/user/image/repository/location/{host_name}-0",
+            "docker_image_tag": "latest",
+            "output_user_bucket": f"output-user-bucket-{uuid}"
+            
+        }
+        # Create a temporary vars.yml file
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".yml", mode='w', dir=INVENTORY_DIR) as vars_file:
+            yaml.dump(vars_content, vars_file, default_flow_style=False)
+            vars_path = vars_file.name
+            print(f"Vars file created at: {vars_path}")
         # Generate the inventory content
         inventory_content = {
             "all": {
@@ -49,11 +65,11 @@ async def run_ansible_playbook(request: PlaybookRequest):
                         "ansible_remote_tmp": "/tmp",
                         "ansible_ssh_common_args": "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null",
                         "ansible_become": "yes"
+                       
                     }
                 }
             }
         }
-
         # Create a temporary inventory file
         with tempfile.NamedTemporaryFile(delete=False, suffix=".yaml", mode='w', dir=INVENTORY_DIR) as inventory_file:
             yaml.dump(inventory_content, inventory_file, default_flow_style=False)
@@ -62,7 +78,7 @@ async def run_ansible_playbook(request: PlaybookRequest):
 
         try:
             # Run the Ansible playbook asynchronously
-            result = await run_ansible_playbook_async(inventory_path)
+            result = await run_ansible_playbook_async(inventory_path, vars_path)
         finally:
             # Clean up the inventory file after execution
             os.remove(inventory_path)
