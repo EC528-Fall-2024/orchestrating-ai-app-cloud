bf45b13f061374c2e05dd9b9abdeb7571db9b688
Kris7180 <113263872+Kris7180@users.noreply.github.com>
2024-12-07 17:53:14 -0500

    Update output_ops.py

--- cynthus_cli/cynthus/output_ops.py
+++ cynthus_cli/cynthus/output_ops.py
@@ -2,17 +2,15 @@ import requests
 import os
 import tarfile
 from pathlib import Path
-from .firebase_auth import *
+from firebase_auth import check_authentication
 
-# Replace with your Cloud Run URL
 API_URL = "https://us-central1-cynthusgcp-438617.cloudfunctions.net/output-ops/generate_workspace_url"
 
 
-def get_signed_url(bucket_name, firebase_token):
+def get_signed_url(firebase_token):
     """Request a signed URL for the compressed workspace folder."""
-    headers = {"Authorization": f"Bearer {firebase_token}"}
-    response = requests.post(API_URL, headers=headers, json={
-                             "bucket_name": bucket_name})
+    headers = {"Auth": f"Bearer {firebase_token}"}
+    response = requests.post(API_URL, headers=headers, json={})
     response.raise_for_status()
     return response.json()["signed_url"]
 
@@ -22,22 +20,23 @@ def download_and_extract(signed_url, output_dir):
     output_path = Path(output_dir)
     output_path.mkdir(parents=True, exist_ok=True)
 
-    archive_path = output_path / "workspace.tar.gz"
+    archive_path = output_path / "output.tar.gz"
 
     # Download the archive
     print("Downloading workspace archive...")
-    response = requests.get(signed_url)
+    response = requests.get(signed_url, stream=True)
     response.raise_for_status()
     with open(archive_path, "wb") as f:
-        f.write(response.content)
+        for chunk in response.iter_content(chunk_size=8192):
+            f.write(chunk)
 
     # Extract the archive
-    print("Extracting workspace archive...")
-    with tarfile.open(archive_path, "r:gz") as tar:
-        tar.extractall(path=output_path)
+    # print("Extracting workspace archive...")
+    # with tarfile.open(archive_path, "r:gz") as tar:
+    #     tar.extractall(path=output_path)
 
-    print(f"Workspace extracted to {output_path}")
-    os.remove(archive_path)  # Clean up the archive file
+    # print(f"Workspace extracted to {output_path}")
+    # os.remove(archive_path)  # Clean up the archive file
 
 
 def pull_output():
@@ -49,11 +48,9 @@ def pull_output():
             raise ValueError(
                 "Authentication failed. Cannot retrieve bucket information.")
 
-        bucket_name = f"output-user-bucket-{uid}"
-
-        signed_url = get_signed_url(bucket_name, token)
+        signed_url = get_signed_url(token)
         download_and_extract(signed_url, output_dir)
 
-        print(f"Workspace downloaded and extracted to {output_dir}")
+        print(f"Workspace downloaded to {output_dir}")
     except Exception as e:
         print(f"Error: {e}")
