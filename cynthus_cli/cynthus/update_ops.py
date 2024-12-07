78765322c72605cb79ef3bba1c95312fd6077941
Kris7180 <113263872+Kris7180@users.noreply.github.com>
2024-12-07 02:15:03 -0500

    Update update_ops.py
    remove emojis from update

--- cynthus_cli/cynthus/update_ops.py
+++ cynthus_cli/cynthus/update_ops.py
@@ -13,10 +13,10 @@ def update_src():
         code_url = 'https://code-update-531274461726.us-east4.run.app'
         code_response = requests.post(code_url, headers=headers)
         code_response.raise_for_status()
-        print('✅ Code update successful:', code_response.json())
+        print('Code update successful:', code_response.json())
 
     except requests.exceptions.RequestException as e:
-        print('❌ Error:', {
+        print('Error:', {
             'message': e.response.json() if hasattr(e, 'response') else str(e),
             'status': e.response.status_code if hasattr(e, 'response') else None
         })
@@ -34,9 +34,9 @@ def update_data():
         data_url = 'https://data-update-531274461726.us-east4.run.app'
         data_response = requests.post(data_url, headers=headers)
         data_response.raise_for_status()
-        print('✅ Data update successful:', data_response.json())
+        print('Data update successful:', data_response.json())
     except requests.exceptions.RequestException as e:
-        print('❌ Error:', {
+        print('Error:', {
             'message': e.response.json() if hasattr(e, 'response') else str(e),
             'status': e.response.status_code if hasattr(e, 'response') else None
         })
